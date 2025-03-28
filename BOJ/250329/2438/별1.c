cafbfd3fcd113ee70fc0f7e3be44e00769daaf91
JH1476 <junghun14760@naver.com>
2025-03-29 02:50:42 +0900

    Create 별1.c

---
+++ BOJ/250329/2438/별1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    for(int i = 0; i < T; i++) {
+        for(int j = 0; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
