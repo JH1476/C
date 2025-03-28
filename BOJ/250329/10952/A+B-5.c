91e8b6ae28f8534dd6219306ff18122a55189d48
JH1476 <junghun14760@naver.com>
2025-03-29 03:51:18 +0900

    Create A+B-5.c

---
+++ BOJ/250329/10952/A+B-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int loop = 1;
+
+    while(loop) {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        if(a==0 && b==0) {
+            return 0;
+        }
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
