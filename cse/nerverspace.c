2baf1542fb601cbd88a27e87850c84062a7be5af
JH1476 <junghun14760@naver.com>
2025-04-12 21:27:37 +0900

    Create nerverspace.c

---
+++ cse/nerverspace.c
@@ -0,0 +1,12 @@
+    #include <stdio.h>
+    #include <string.h>
+    int main() {
+        char str[99];
+        scanf("%[^\n]s", str);
+        for(int i = 0; i < strlen(str); i++){
+            if(str[i] != ' '){
+                printf("%c", str[i]);
+            }
+        }
+        return 0;
+    }
